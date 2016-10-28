981553db79662cc4c4d2d561f9108984a721b8c4
SRenan <renan.sauteraud@gmail.com>
2016-10-27 20:47:44 -0400

    XCIR for phased samples
    Add reading of shapeit2 output
    Add annotation from seqminer
    Add function to get coverage per gene
    Improve doc
    Add Improts in DESCRIPTION
    Add various options
    
    
    Former-commit-id: 4645199428c9d42616a3bcf55116e63920542813 [formerly b8b19bad367e5d821d49bbbfeac1f3bd33a8faf2]
    Former-commit-id: 4d9cd576ffab878a471a8df0d2de0e47ba62e99e

--- R/XCI.R
+++ R/XCI.R
@@ -1,40 +1,55 @@
+#' Get sum of the coverage of all SNPs for each gene
+#'
+#' @importFrom ggplot2 ggplot geom_histogram xlab ylab
 #' @export
-getGenicDP <- function(dt_anno, gender_file){
-  sex <- fread(gender_file)
-  setnames(sex, c("subject", "gender"))
-  sex[, gender := tolower(gender)]
-  dt_anno <- merge(dt_anno, sex, by = "subject")
-  tot <- dt_anno[, lapply(.SD, sum, na.rm = TRUE), by = c("CHROM", "gender", "GENE", "subject"), .SDcols = c("AD_ref", "AD_alt")] #nrow = nGenes * nSubs
+getGenicDP <- function(dt_anno, plot = FALSE, gender_file = NULL){
+  if(!is.null(gender_file)){
+    sex <- fread(gender_file)
+    setnames(sex, c("sample", "gender"))
+    sex[, gender := tolower(gender)]
+    dt_anno <- merge(dt_anno, sex, by = "sample")
+  }
+  else if(!"gender" %in% names(dt_anno)){
+    stop("If there is no gender column in the input, a gender_file mapping
+         sample to gender should be provided")
+  }
+  tot <- dt_anno[, lapply(.SD, sum, na.rm = TRUE), by = c("CHROM", "gender", "GENE", "sample"), .SDcols = c("AD_ref", "AD_alt")] #nrow = nGenes * nSubs
   tot[, tot := AD_ref + AD_alt]
+  if(plot){
+    ggplot(tot[, mean(tot), by = "GENE"], aes(V1)) + geom_histogram() + xlab(label = "coverage") + ylab("gene count")
+  }
   return(tot)
 }
 
-# Input:
-# The output of the estimated genic DP
-# A list of genes that are known to NOT escape XCI (or a file).
-# Output:
-#
-#dt <- data.table(tot)
-#xciGenes <- "data/startdata/xciGene.txt"
-
 #' Get fraction of cell expressing the inactivated allele
 #'
-#' @param dt A \code{data.table}. The data.
+#' @param dt A \code{data.table}. The output of \code{getGenicDP}.
 #' @param xciGenes A \code{character}. The name of the genes to analyze.
 #'
+#'
+#' @details
+#' The default xciGenes is a list of 177 genes known to be affected by X
+#' chromosome inactivation from Cotton et al. 2013
+#' (dx.doi.org/10.1186/gb-2013-14-11-r122).
+#'
 #' @return A \code{data.table} containing the average, and median fraction of
-#' expression of the inactivated chromosome for each subject.
+#' expression of the inactivated chromosome for each sample. frac_XIST is the
+#' fraction of total XIST expression contributed by the inactive X chromosome.
+#' @seealso getGenicDP
+#'
 #' @export
-getCellFrac <- function(dt, xciGenes){
+getCellFrac <- function(dt, xciGenes = NULL){
+  if(is.null(xciGenes))
+    xciGenes <- system.file("extdata", "xciGene.txt", package = "XCIR")
   xci <- readLines(xciGenes)
   dt <- dt[GENE %in% c(xci, "XIST"),]
   dt <- dt[, frac := AD_ref/tot]
   dt <- dt[frac > .5, frac := 1-frac] #Set fraction that are above .5 to 1-frac
   dt <- dt[tot < 10, tot := NA]
-  dt <- dt[, frac_mean := mean(frac, na.rm = TRUE), by = "subject"]
-  dt <- dt[, frac_median := median(frac, na.rm = TRUE), by = "subject"]
+  dt <- dt[, frac_mean := mean(frac, na.rm = TRUE), by = "sample"]
+  dt <- dt[, frac_median := median(frac, na.rm = TRUE), by = "sample"]
 
-  dt_frac <- unique(dt[GENE == "XIST", list(subject, frac, frac_median, frac_mean)])
+  dt_frac <- unique(dt[GENE == "XIST", list(sample, frac, frac_median, frac_mean)])
   setnames(dt_frac, "frac", "frac_XIST")
   return(dt_frac) #This is fracCell.allXci.txt
 }
@@ -61,31 +76,36 @@ getCellFrac <- function(dt, xciGenes){
 #'
 #' @param dt A \code{data.table}. The data. It must contain a gender column.
 #' @param dt_frac A \code{data.table}. The result of the \code{getCellFrac}
-#' function.
+#'  function.
 #' @param dp_max A \code{numeric}. If any gene has a total coverage above this
-#' number, the coverage will be set to this number.
+#'  number, the coverage will be set to this number.
+#' @param output A \code{logical}. If set to TRUE, output a table for each
+#'  cutoff level.
 #'
 #' @export
-getXIexpr <- function(dt, dt_frac, xciGenes, dp_max = 500, output = TRUE){
+getXIexpr <- function(dt, dt_frac, xciGenes = NULL, dp_max = 500, output = FALSE){
+  if(is.null(xciGenes))
+    xciGenes <- system.file("extdata", "xciGene.txt", package = "XCIR")
   xci <- readLines(xciGenes)
   dt[, frac := AD_ref/tot]
   dt <- dt[GENE %in% xci]
   dt <- dt[tolower(gender) == "female"]
   dt[tot > dp_max, AD_ref := frac*dp_max]
   dt[tot > dp_max, tot := dp_max]
-  full_dt <- merge(dt, dt_frac, by = "subject")
+  full_dt <- merge(dt, dt_frac, by = "sample")
   cutoffs <- seq(50, 10, -10)
   l <- vector("list", length(cutoffs))
   i <- 1
+  #At each cutoff, consider only the genes that have a total read count higher than the cutoff
   for(dp_cutoff in cutoffs){
     # Anything that depends on the coverage is affected by the cutoff
     dt <- copy(full_dt)
     dt[, cutoff := dp_cutoff]
     dt[AD_ref < dp_cutoff, AD_ref := NA]
     dt[tot < dp_cutoff, tot := NA]
-    dt[, pI := (AD_ref/tot + frac_mean - 1)/(2*frac_mean-1), by = c("GENE", "subject")]
-    dt[, sd_pI := sqrt((AD_ref/tot)*(1-AD_ref/tot)/(tot))/abs(2*frac_mean-1), by = c("GENE", "subject")]
-    dt[, pval_pI := pnorm(pI/sd_pI, mean = 0, lower.tail = FALSE), by = c("GENE", "subject")]
+    dt[, pI := (AD_ref/tot + frac_mean - 1)/(2*frac_mean-1), by = c("GENE", "sample")]
+    dt[, sd_pI := sqrt((AD_ref/tot)*(1-AD_ref/tot)/(tot))/abs(2*frac_mean-1), by = c("GENE", "sample")]
+    dt[, pval_pI := pnorm(pI/sd_pI, mean = 0, lower.tail = FALSE), by = c("GENE", "sample")]
     dt[, t := pI/sd_pI]
     dt[, normExp := qnorm(pval_pI, lower.tail = FALSE)]
     if(output)
@@ -96,8 +116,3 @@ getXIexpr <- function(dt, dt_frac, xciGenes, dp_max = 500, output = TRUE){
   return(rbindlist(l))
 }
 
-#' @export
-write_siteRef <- function(dt, ref_file = "test_ref.anno.tsv"){
-  write.table(dt[, list(CHROM, POS, REF, ALT)], file = ref_file,
-              row.names = FALSE, quote = FALSE, sep = "\t")
-}
