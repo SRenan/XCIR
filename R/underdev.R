ac1633bd5678ea5fc35c82b01d1ca2afe1fef1c9
SRenan <renan.sauteraud@gmail.com>
2018-03-06 11:50:50 -0500

    Enable model specification
    Improve model checking
    
    
    Former-commit-id: 6e8b8deb170b3e014d6123a6194cc7a89b628b3c [formerly 8a2e566f0dd9d16267108065d2d7fbae02f14502]
    Former-commit-id: f36deb41a6976f03aff78a4a060d9a68d7087346

--- R/underdev.R
+++ R/underdev.R
@@ -1,2 +1,9 @@
 # This file for functions under development before they get added to the files where they belong
 
+getZ <- function(xcirout){
+  Zdt <- copy(xcirout)
+  Zdt[, mup := mean(p_value), by = "GENE"]
+  Zdt[, sdp := sd(p_value), by = "GENE"]
+  Zdt[, Z := (p_value - mup)/sdp]
+
+}
